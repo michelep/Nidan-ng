9db6d296122a9ecc391b967c9a7de1a8e151405d
Michele Pin <o-zone@zerozone.it>
2021-03-10 21:30:52 +0000

    license and other

--- snmp.php
+++ snmp.php
@@ -1,7 +1,19 @@
 <?php
-
 //
-// Questa procedura preleva dai routers, via SNMP, l'elenco degli indirizzi IP connessi
+// Nidan-ng
+// LAN monitoring system
+//
+// by Michele "O-Zone" Pinassi
+//
+// This file is part of Nidan-ng
+//
+// Nidan-ng is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or any later version.
+//
+// Nidan-ng is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with Nidan-ng. If not, see <http://www.gnu.org/licenses/>.
 //
 
 include_once __DIR__."/common.inc.php";
